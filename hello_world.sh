17d238f6ce7417251368258518eb2a7ef6f4ff96
Abbas Ismael <aismoctar@gmail.com>
2025-05-06 12:27:41 -0800

    verion initiale

---
+++ hello_world.sh
@@ -0,0 +1,8 @@
+#!/bin/bash # Indique l'interpreteur a utiliser(Pour notre cas bash)
+
+# ce script affiche Hello World
+echo "Hello World"
+
+# droit d'execution
+chmod +x hello_world.sh #rendre le fichier executable
+
