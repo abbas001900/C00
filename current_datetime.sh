28eaa2fc2b60aff8b8847156f35933dbb5000f8a
Abbas Ismael <aismoctar@gmail.com>
2025-05-06 12:26:18 -0800

    verion initiale

---
+++ current_datetime.sh
@@ -0,0 +1,7 @@
+#!/bin/bash # Indique l'interpreteur a utiliser(Pour notre cas bash)
+
+# Affichage de la date et l'heure sous format YYYY-MM-DD HH:MM:SS
+date +"%Y-%m-%d %H:%M:%S"
+
+#Rendre le fichier executable
+chmod +x current_datetime.she
