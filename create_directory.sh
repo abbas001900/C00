8bef157fe33792ac1605a48a9cf47455d1ea4577
Abbas Ismael <aismoctar@gmail.com>
2025-05-06 12:23:19 -0800

    Version initiale

---
+++ create_directory.sh
@@ -0,0 +1,7 @@
+#!/bin/bash # Indique l'interpreteur a utiliser(Pour notre cas bash)
+
+# creation du dossier 'test_directory'
+mkdir test_directory 
+
+# Rendre le fichier executable
+chmod +x create_directory.sh
