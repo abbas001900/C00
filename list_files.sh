c1f6118b51f39f953b6446c930bf7ac86591683b
Abbas Ismael <aismoctar@gmail.com>
2025-05-06 12:33:25 -0800

    Create list_files.sh

---
+++ list_files.sh
@@ -0,0 +1,14 @@
+#!/bin/bash # Indique l'interpreteur a utiliser(Pour notre cas bash)
+
+: << 'COMMENTAIRE'
+Nous allons ensuite faire une boucle qui recuperera uniquement
+les fichiers se trouvant dans le current directory
+COMMENTAIRE
+
+for item in *; do # condition de la boucle for
+        if [ -f "$item" ]; then # condition du if
+                ls "$item" # requete a effectuer si la condition est respecter
+        fi # marque la fin du if
+done # marque la fin du for
+
+chmod +x list_files.sh #rendre le fichier executable
